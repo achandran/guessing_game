9123bd34eba9704a4c8b78954af44c84e4108cfd
Anand Chandran <hello@achandran.com>
2016-05-11 19:10:04 -0400

    Add some comments and clean up some code

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,19 @@ use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
-    println!("Guess the number!");
+    println!("Guess the number from 1 through 100");
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
     loop {
         println!("Please input your guess.");
+        // by default bindings in rust are immutable so we add mut
         let mut guess = String::new();
 
         io::stdin().read_line(&mut guess)
             .expect("Failed to read line");
 
+        // when user enters non number, ask for input again
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_)  => continue,
